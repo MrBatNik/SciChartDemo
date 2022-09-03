5be18e3aa0ff4b39ccf5a443d8bf3eac7edf204c
Nikita <nicandreo@gmail.com>
2022-09-03 15:03:24 +0300

    Initial Commit

---
+++ SciChartDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SciChartDemo
+//
+//  Created by Nikita Batrakov on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
